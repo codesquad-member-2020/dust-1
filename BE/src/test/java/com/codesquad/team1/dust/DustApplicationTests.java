e42a731e86b5dbce7a6098abf33f2713cc2cbd9a
Jay <beginin15@gmail.com>
2020-03-30 20:48:23 +0900

    [#20] Feat: 프로젝트 생성
    - 스프링 부트 프로젝트 생성
    - gitignore 파일 설정

---
+++ BE/src/test/java/com/codesquad/team1/dust/DustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.team1.dust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DustApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
