73177bececd3def3834db3e66b69d51f3d9d41c0
dev-Lena <keunnalee@gmail.com>
2020-03-31 02:50:03 +0900

    [#22] Feat: 예보 탭 UI 구현

---
+++ iOS/dustProject/dustProject/Controller/ForecastViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForecastViewController.swift
+//  DustProject
+//
+//  Created by Keunna Lee on 2020/03/31.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iOS/dustProject/dustProject/Controller/MicroDustViewController.swift
+++ iOS/dustProject/dustProject/Controller/MicroDustViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MicroDustViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
