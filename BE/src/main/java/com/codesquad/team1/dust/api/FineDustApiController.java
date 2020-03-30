af90a5fdc37760dec16e33bd3120be4bf7e94070
IdionKim <ksundong@gmail.com>
2020-03-30 23:14:35 +0900

    [#32] Feat: GET locations Mock API 구현
     - StationLocation 클래스를 추가하였습니다. 해당 클래스는 stationName과 location 값을 가집니다.
     - FineDustApiController 클래스를 추가하였습니다. 해당 클래스는 RestController이며, json Array 형태의 데이터를 반환합니다.
     - Close #32 Issue

---
+++ BE/src/main/java/com/codesquad/team1/dust/api/FineDustApiController.java
@@ -0,0 +1,29 @@
+package com.codesquad.team1.dust.api;
+
+import com.codesquad.team1.dust.domain.StationLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class FineDustApiController {
+
+    private static final Logger log = LoggerFactory.getLogger(FineDustApiController.class);
+
+    @GetMapping("/locations")
+    public List<StationLocation> showStationLocations() {
+        List<StationLocation> stationLocations = new ArrayList<>();
+
+        stationLocations.add(new StationLocation("강남구", "서울 강남구 학동로 426강남구청 별관 1동"));
+        stationLocations.add(new StationLocation("구로구", "서울 구로구 가마산로 27길 45구로고등학교"));
+
+        log.debug("list: {}", stationLocations);
+
+        return stationLocations;
+    }
+
+}
