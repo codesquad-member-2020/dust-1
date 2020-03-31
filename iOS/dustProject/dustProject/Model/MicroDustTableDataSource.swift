f579fed49edeed723da7b80f3cb3d7e2c2d79270
dev-Lena <keunnalee@gmail.com>
2020-03-31 17:21:57 +0900

    [#25] Feat: 미세먼지탭 하단 테이블 뷰 관련 클래스 생성
    - TableViewDataSource 클래스인 MicroDustTableDataSource 클래스 생성
    - ViewController에서 TableView의 data source로 MicroDustTableDataSource 객체 지정

--- iOS/dustProject/dustProject/Model/MicroDustTableDataSource.swift
+++ iOS/dustProject/dustProject/Model/MicroDustTableDataSource.swift
@@ -1,9 +1,23 @@
 //
-//  File.swift
+//  MicroDustTableDataSource.swift
 //  DustProject
 //
 //  Created by Keunna Lee on 2020/03/31.
 //  Copyright © 2020 dev-Lena. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MicroDustTableDataSource: NSObject, UITableViewDataSource {
+    
+    var dustInfo : [MicroDustInfo] = []
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "microDustCell", for: indexPath)
+        let dataForNow = dustInfo[indexPath.row]
+        return cell
+    }
+}
