49a1ca84c31c6a89316b75e47f18290988ef1123
IdionKim <ksundong@gmail.com>
2020-03-31 21:14:08 +0900

    [#59] Feat: 미세먼지 예보 API 요청 후 해당 정보를 Parsing하여 원하는 형태로 반환하는 API 구현
     - 기존 forecast와 반환하는 데이터 형식은 동일합니다.
     - 로깅을 하도록 구현하였습니다.
     - 서비스 키는 상수로 관리하고, 요청 URL 및 파라미터 Key들도 상수로 관리할 수 있으면 상수로 처리하였습니다.
     - Close #59 Issue.

--- BE/src/main/java/com/codesquad/team1/dust/domain/Forecast.java
+++ BE/src/main/java/com/codesquad/team1/dust/domain/Forecast.java
@@ -1,5 +1,8 @@
 package com.codesquad.team1.dust.domain;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Forecast {
@@ -8,10 +11,23 @@ public class Forecast {
     private String informGrade;
     private List<Image> images;
 
-    public Forecast(String informOverall, String informGrade, List<Image> images) {
-        this.informOverall = informOverall;
-        this.informGrade = informGrade;
-        this.images = images;
+    public Forecast(JSONObject forecastObject) {
+        this.informOverall = forecastObject.getString("informOverall");
+        this.informGrade = forecastObject.getString("informGrade");
+        this.images = parseImages(forecastObject);
+    }
+
+    private List<Image> parseImages(JSONObject forecastObject) {
+        List<Image> images = new ArrayList<>();
+
+        images.add(new Image(forecastObject.getString("imageUrl1")));
+        images.add(new Image(forecastObject.getString("imageUrl2")));
+        images.add(new Image(forecastObject.getString("imageUrl3")));
+        images.add(new Image(forecastObject.getString("imageUrl4")));
+        images.add(new Image(forecastObject.getString("imageUrl5")));
+        images.add(new Image(forecastObject.getString("imageUrl6")));
+
+        return images;
     }
 
     public String getInformOverall() {
