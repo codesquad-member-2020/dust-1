25ca4ddb6e11da69cd4cd3a6555ddb7800d83cb3
IdionKim <ksundong@gmail.com>
2020-04-01 18:09:41 +0900

    [#62] Feat: daily-dust-status API 구현
     - 기존 daily-dust-status 와 반환하는 데이터 형식은 동일합니다.
     - 로깅을 하도록 구현하였습니다.
     - Fixed #62 Issue.

--- BE/src/main/java/com/codesquad/team1/dust/util/PublicAPIUtils.java
+++ BE/src/main/java/com/codesquad/team1/dust/util/PublicAPIUtils.java
@@ -1,5 +1,7 @@
 package com.codesquad.team1.dust.util;
 
+import com.codesquad.team1.dust.domain.DustStatus;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,30 +9,51 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
-import static com.codesquad.team1.dust.constants.CommonConstants.AND_RETURN_TYPE_JSON;
-import static com.codesquad.team1.dust.constants.CommonConstants.AND_SERVICE_KEY;
-import static com.codesquad.team1.dust.constants.CommonConstants.PUBLIC_API_FORECAST_URL_AND_SEARCH_DATE;
-import static com.codesquad.team1.dust.constants.CommonConstants.PUBLIC_API_SERVICE_KEY;
+import static com.codesquad.team1.dust.constants.CommonConstants.*;
 
 public class PublicAPIUtils {
 
     private static final Logger log = LoggerFactory.getLogger(PublicAPIUtils.class);
+    private static final RestTemplate restTemplate = new RestTemplate();
 
     public static JSONObject getForecastJSONObject(String searchDate) throws URISyntaxException {
+        log.debug("검색한 날짜: {}", searchDate);
         URI publicApiRequestUrl = new URI(PUBLIC_API_FORECAST_URL_AND_SEARCH_DATE + searchDate
                 + AND_SERVICE_KEY + PUBLIC_API_SERVICE_KEY + AND_RETURN_TYPE_JSON);
-
-        RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject(publicApiRequestUrl, String.class);
-        JSONObject forecastObject = new JSONObject(response).getJSONArray("list").getJSONObject(0);
-
-        log.debug("검색한 날짜: {}", searchDate);
         log.debug("requestUrl: {}", publicApiRequestUrl);
+
+        String response = restTemplate.getForObject(publicApiRequestUrl, String.class);
         log.debug("response: {}", response);
+        JSONObject forecastObject = new JSONObject(response).getJSONArray("list").getJSONObject(0);
         log.debug("forecastJSONObject: {}", forecastObject);
 
         return forecastObject;
     }
 
+    public static List<DustStatus> getDailyDustStatusJSONArray(String stationName) throws URISyntaxException {
+        List<DustStatus> dustStatusList = new ArrayList<>();
+        URI publicApiRequestUrl = new URI(PUBLIC_API_REALTIME_MEASURE_URL + stationName
+                + AND_DATA_TERM_DAILY + AND_PAGE_NO_1 + AND_NUM_OF_ROWS_24 + AND_SERVICE_KEY + PUBLIC_API_SERVICE_KEY
+                + AND_VERSION + 1.3 + AND_RETURN_TYPE_JSON);
+        log.debug("requestUrl: {}", publicApiRequestUrl);
+
+        String response = restTemplate.getForObject(publicApiRequestUrl, String.class);
+        log.debug("response: {}", response);
+
+        JSONArray dustStatuses = new JSONObject(response).getJSONArray("list");
+        log.debug("dustStatuses: {}", dustStatuses);
+
+        for (Object dustStatusObject : dustStatuses) {
+            JSONObject dustStatusJSONObject = (JSONObject) dustStatusObject;
+            dustStatusList.add(new DustStatus(dustStatusJSONObject));
+        }
+        log.debug("dustStatusList: {}", dustStatusList);
+
+        return dustStatusList;
+    }
+
+    private PublicAPIUtils() {}
 }
