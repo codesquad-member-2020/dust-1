3cd2ef8108ae00085260cb153ff4b26d5ab8e765
최성익 <tjddlr92@gmail.com>
2020-04-02 13:55:32 +0900

    [#13] Feat: 예보 정보 렌더링
    - 예보 데이터 보여주기
    - 이미지 작업은 x
    - 예보 내용 스타일 약간 변경

--- FE/src/js/components/forecast.js
+++ FE/src/js/components/forecast.js
@@ -1,4 +1,39 @@
 import { _q, addClass, clearClass } from "../utils/utils";
-import { CLASS_NAME } from "../utils/constants";
+import { CLASS_NAME, FORECAST_IMG_ALT } from "../utils/constants";
 
 export const forecastContents = _q(`.${CLASS_NAME.forecastContents}`);
+
+const forecastElements = {
+  images: _q(`.${CLASS_NAME.forecastImages}`),
+  inform: _q(`.${CLASS_NAME.forecastInform}`),
+  progressBarIcon: _q(`${CLASS_NAME.progressBarIcon}`),
+};
+
+const forecastInformContent = (informOverall, informGrade) => `<p class="${CLASS_NAME.forecastInformOverall}">${informOverall}</p><p class="${CLASS_NAME.forecastInformGrade}">${informGrade}</p>`;
+
+const forecastImageContent = images =>
+  images.reduce((imageElement, currentImage) => {
+    const { imageUrl } = currentImage;
+    imageElement += `<image class="${CLASS_NAME.forecastImage}" src="${imageUrl}" alt="${FORECAST_IMG_ALT}">`;
+    return imageElement;
+  }, "");
+
+const selectViewImage = () => {};
+
+export const renderForecast = forecastData => {
+  const { informOverall, informGrade, images } = forecastData;
+  forecastElements.inform.innerHTML = forecastInformContent(informOverall, informGrade);
+  forecastElements.images.innerHTML = forecastImageContent(images);
+};
+
+const changePlayButtonIcon = () => {
+  //  play_arrow or pause
+};
+
+const calculateProgressBarWidth = () => {};
+
+const addPlayButtonTouchEvent = () => {};
+
+const addControlButtonTouchEvent = () => {};
+
+const controlImage = () => {};
