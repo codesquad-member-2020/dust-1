4cd1b45d502c9ff128d583dae317176c1a5c3514
Jay <beginin15@gmail.com>
2020-04-03 00:17:09 +0900

    [#82] Fix: 이미지 파일 리스트 정렬 문제 해결
    - gif 변환 프로그램 실행 시 주는 파일이름 옵션으로 해결

--- BE/src/main/java/com/codesquad/team1/dust/util/ImageResourceUtils.java
+++ BE/src/main/java/com/codesquad/team1/dust/util/ImageResourceUtils.java
@@ -4,22 +4,22 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ImageResourceUtils {
 
     private static final Logger log = LoggerFactory.getLogger(ImageResourceUtils.class);
-    private static final String DIR_IMAGES_PATH = System.getProperty("user.dir") + "/src/main/resources/static/images";
-    private static final String IMAGE_URI = "http://ec2-15-164-254-158.ap-northeast-2.compute.amazonaws.com:8080/images/";
-    private static final String IMAGE_PREFIX = "forecast";
-    private static final int MAX_IMAGES = 48;
+    private static final String IMAGES_DIR_PATH = System.getProperty("user.dir") + "/src/main/resources/static/images";
+    private static final String IMAGES_URL_PATH = "http://ec2-15-164-254-158.ap-northeast-2.compute.amazonaws.com:8080/images/";
 
     public static List<String> parseImageURLs(JsonNode forecastObject) {
         log.debug("pm10 gif imageUrl7 : {}", forecastObject.get("imageUrl7"));
-        log.debug("resource path : {}", DIR_IMAGES_PATH);
-        excuteGIFSplit(String.format("convert %s %s/%s.png", forecastObject.get("imageUrl7"), DIR_IMAGES_PATH, IMAGE_PREFIX));
+        log.debug("resource path : {}", IMAGES_DIR_PATH);
+        excuteGIFSplit(String.format("convert %s %s/forecast-%%02d.png", forecastObject.get("imageUrl7"), IMAGES_DIR_PATH));
         return getImageURLs();
     }
 
@@ -55,9 +55,18 @@ public class ImageResourceUtils {
     }
 
     private static List<String> getImageURLs() {
+        File[] files = new File(IMAGES_DIR_PATH).listFiles();
+        return (files == null) ? new ArrayList<>() : getOrderedImageURLs(files);
+    }
+
+    private static List<String> getOrderedImageURLs(File[] files) {
         List<String> imageURLs = new ArrayList<>();
-        for (int i = 0; i < MAX_IMAGES; ++i)
-            imageURLs.add(String.format("%s%s-%d.png", IMAGE_URI, IMAGE_PREFIX, i));
+        for (File file : files) {
+            String fileName = file.getName();
+            if (fileName.contains(".png"))
+                imageURLs.add(IMAGES_URL_PATH + fileName);
+        }
+        Collections.sort(imageURLs);
         return imageURLs;
     }
 
