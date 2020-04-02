e4bdf4da917b40dfd44a924411b5d2ec2e784280
최성익 <tjddlr92@gmail.com>
2020-04-02 16:43:11 +0900

    [#13] Refactor: tab 엘리먼트 객체로 변환
    - 다른 파일과의 일관성을 위해 객체에 엘리먼트를 담게 변경했다.

--- FE/src/js/components/tab.js
+++ FE/src/js/components/tab.js
@@ -3,22 +3,24 @@ import { _q, reverseClass } from "../utils/utils";
 import { dustContents } from "./dustStatus";
 import { forecastContents } from "./forecast";
 
-const tabs = _q(`.${CLASS_NAME.tabs}`);
-const dustTab = _q(`.${CLASS_NAME.dustTab}`);
-const forecastTab = _q(`.${CLASS_NAME.forecastTab}`);
+const tabElem = {
+  tabs: _q(`.${CLASS_NAME.tabs}`),
+  dustTab: _q(`.${CLASS_NAME.dustTab}`),
+  forecastTab: _q(`.${CLASS_NAME.forecastTab}`),
+};
 
 const changeTab = event => {
   event.preventDefault();
-  if (event.target === dustTab) {
-    reverseClass(CLASS_NAME.active, dustTab, forecastTab);
+  if (event.target === tabElem.dustTab) {
+    reverseClass(CLASS_NAME.active, tabElem.dustTab, tabElem.forecastTab);
     reverseClass(CLASS_NAME.hidden, forecastContents, dustContents);
     return;
   }
-  reverseClass(CLASS_NAME.active, forecastTab, dustTab);
+  reverseClass(CLASS_NAME.active, tabElem.forecastTab, tabElem.dustTab);
   reverseClass(CLASS_NAME.hidden, dustContents, forecastContents);
 };
 
 export default () => {
-  tabs.addEventListener("touchend", event => changeTab(event));
-  tabs.addEventListener("click", event => changeTab(event));
+  tabElem.tabs.addEventListener("touchend", event => changeTab(event));
+  tabElem.tabs.addEventListener("click", event => changeTab(event));
 };
