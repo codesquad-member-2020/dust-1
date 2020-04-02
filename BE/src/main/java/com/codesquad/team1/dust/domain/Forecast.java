c2224740279c4427e759a978b326bdec9e89ec99
IdionKim <ksundong@gmail.com>
2020-04-02 16:06:20 +0900

    [#83] Refactor: org.json to jackson library migration
     - jackson 라이브러리가 더 좋다고 해서 변경하였습니다.
     - 코드 구조가 크게 바뀌진 않았습니다.
     - org.json 의존성을 제거하였습니다.
     - Close #83 Issue.

--- BE/src/main/java/com/codesquad/team1/dust/domain/Forecast.java
+++ BE/src/main/java/com/codesquad/team1/dust/domain/Forecast.java
@@ -1,6 +1,6 @@
 package com.codesquad.team1.dust.domain;
 
-import org.json.JSONObject;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,18 +11,18 @@ public class Forecast {
     private String informGrade;
     private List<Image> images;
 
-    public Forecast(JSONObject forecastObject) {
-        this.informOverall = forecastObject.getString("informOverall");
-        this.informGrade = forecastObject.getString("informGrade");
+    public Forecast(JsonNode forecastObject) {
+        this.informOverall = forecastObject.get("informOverall").asText();
+        this.informGrade = forecastObject.get("informGrade").asText();
         this.images = parseImages(forecastObject);
     }
 
-    private List<Image> parseImages(JSONObject forecastObject) {
+    private List<Image> parseImages(JsonNode forecastObject) {
         List<Image> images = new ArrayList<>();
 
-        images.add(new Image(forecastObject.getString("imageUrl1")));
-        images.add(new Image(forecastObject.getString("imageUrl2")));
-        images.add(new Image(forecastObject.getString("imageUrl3")));
+        images.add(new Image(forecastObject.get("imageUrl1").asText()));
+        images.add(new Image(forecastObject.get("imageUrl2").asText()));
+        images.add(new Image(forecastObject.get("imageUrl3").asText()));
 
         return images;
     }
