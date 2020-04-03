2569a5fd052cda0089fc2295319a50e31cf9217e
dev-Lena <keunnalee@gmail.com>
2020-04-03 17:09:20 +0900

    [#98] Feat: 버튼을 누르면 대기 상태 이미지 재생 기능 추가
    - 플레이 버튼을 누르면 Slider 위치가 움직임
    - 재생시 API에서 받아온 이미지 애니메이션 재생

--- iOS/dustProject/dustProject/Controller/ForecastViewController.swift
+++ iOS/dustProject/dustProject/Controller/ForecastViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class ForecastViewController: UIViewController {
     
-    @IBOutlet var imageProgressView: UIProgressView!
+    @IBOutlet var imageSlider: UISlider!
     @IBOutlet var informOverall: UILabel!
     @IBOutlet var informGrade: UILabel!
     @IBOutlet var microDustImageView: UIImageView!
+    @IBAction func runButtonTapped(_ sender: Any) {
+        self.progressBarTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.sliderValueChanged(_:)), userInfo: nil, repeats: true)
+        
+    }
     private var progressBarTimer : Timer!
     private let progress = Progress(totalUnitCount: 10)
     private let forecastDataManager = ForecastDataManager()
     private var microDustImageInfo : [ImageInfo]?
-    private var a = [String]()
+    private var isRunning = true
+    var imagesArray = [UIImage]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageProgressView.progress = 0.0
-        self.progressBarTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateImages), userInfo: nil, repeats: true)
-        
+        imageSlider.value = 0.0
         forecastDataManager.decodeMicroDustImageJson()
         guard let imageInfos = forecastDataManager.giveMicroDustImage() else { return }
         self.microDustImageInfo = imageInfos
@@ -32,22 +36,28 @@ class ForecastViewController: UIViewController {
         self.informOverall.text = informOverall
         let informGrade = forecastDataManager.giveInformGrade()
         self.informGrade.text = informGrade
-        
     }
     
-    @objc private func updateImages() {
-        var imagesArray = [UIImage]()
+    @IBAction func sliderValueChanged(_ sender: Any) {
         DispatchQueue.main.async {
-            do { try? self.microDustImageInfo?.forEach { microDustImage in
-                guard let url = URL(string: "\(microDustImage.imageUrl)") else { return }
+            self.imageSlider.value += 0.1
+            do { try? self.microDustImageInfo?.forEach{
+                image in
+                guard let url = URL(string: "\(image.imageUrl)") else { return }
                 let imageData = try Data(contentsOf: url)
                 guard let images = UIImage(data: imageData) else { return }
                 self.microDustImageView.image = images
-                imagesArray.append(images)
-                self.microDustImageView.animationImages = imagesArray
-                self.microDustImageView.animationDuration = 5.0
+                self.imagesArray.append(images)
+                self.microDustImageView.animationImages = self.imagesArray
+                self.microDustImageView.animationDuration = 10.0
                 self.microDustImageView.startAnimating()
-                
+                if(self.imageSlider.value
+                    == 1.0)
+                {
+                    self.progressBarTimer.invalidate()
+                    self.microDustImageView.stopAnimating()
+                    self.isRunning = false
+                }
                 }
             }
         }
